9a17879bbf97c2ca1035323933289a038562e6e4
JEFE DE DESARROLLO <jdesarrollo@JEFEDESARROLLO.commerk.local>
2024-03-20 21:37:00 -0500

    login 5

--- servidor/index.js
+++ servidor/index.js
@@ -321,7 +321,8 @@ app.post("/login", async (req, res) => {
     const match = await bcrypt.compare(password, users.rows[0].password);
     if (match) {
       //create a jwt token
-      const serviceToken = jwt.sign({ id_usuario: users.rows[0].id }, 'my_secret_key', { expiresIn: '12h' });
+      const serviceToken = jwt.sign({ id_usuario: users.rows[0].id_usuario }, 'my_secret_key', { expiresIn: '12h' });
+      console.log("serviceToken", serviceToken);
       res.json({ user: users.rows[0], serviceToken, })
     } else {
       res.status(401).json({ message: 'Invalid Password' })
